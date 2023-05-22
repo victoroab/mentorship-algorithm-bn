6c9979e622c758302ddea72d294f6dfdedf770fd
victoroab <96608980+victoroab@users.noreply.github.com>
2023-05-22 16:45:19 +0100

    refactor 1.1

--- Server/src/controllers/students/findMatchHandler.ts
+++ Server/src/controllers/students/findMatchHandler.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from 'express'
-import log from '../../config/logger'
-import { findMatchService } from '../../services/students/findMatchService'
+// import { Request, Response } from 'express'
+// import log from '../../config/logger'
+// import { findMatchService } from '../../services/students/findMatchService'
 
-export const findMatchHandler = async (
-  req: Request<{ menteeId: string }, {}, {}>,
-  res: Response
-) => {
-  try {
-    const menteeId = req.params.menteeId
-    const matches = await findMatchService(menteeId)
-    res.json(matches)
-  } catch (e) {
-    log.error(e)
-    return res.sendStatus(409)
-  }
-}
+// export const findMatchHandler = async (
+//   req: Request<{ menteeId: string }, {}, {}>,
+//   res: Response
+// ) => {
+//   try {
+//     const menteeId = req.params.menteeId
+//     const matches = await findMatchService(menteeId)
+//     res.json(matches)
+//   } catch (e) {
+//     log.error(e)
+//     return res.sendStatus(409)
+//   }
+// }
