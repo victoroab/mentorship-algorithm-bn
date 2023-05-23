72772b40e36932d0be87abe0f5f8be5568aff441
victoroab <96608980+victoroab@users.noreply.github.com>
2023-05-23 07:17:42 +0100

    cors mismatch fix

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -5,6 +5,7 @@ import log from './config/logger'
 import express, { Request, Response, NextFunction } from 'express'
 import routes from './routes'
 import multer from 'multer'
+import { credentials } from './middware/credentials'
 
 const app = express()
 const PORT = parseInt(process.env.PORT as string) || 3500
@@ -12,25 +13,35 @@ const PORT = parseInt(process.env.PORT as string) || 3500
 export const storage = multer.memoryStorage()
 export const upload = multer({ storage: storage })
 
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3500',
-      'http://localhost:5173',
-      'https://fypplayground-client-dn13n5d7y-victoroab.vercel.app/',
-    ],
-    credentials: true,
-  })
-)
+const corsOptions = {
+  origin: [
+    'https://fypplayground-client-oms7b92ne-victoroab.vercel.app/',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+}
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://fypplayground-client-dn13n5d7y-victoroab.vercel.app/'
-  )
-  next()
-})
+app.use(cors(corsOptions))
+
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:3000',
+//       'http://localhost:3500',
+//       'http://localhost:5173',
+//       'https://fypplayground-client-dn13n5d7y-victoroab.vercel.app/',
+//     ],
+//     credentials: true,
+//   })
+// )
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.header(
+//     'Access-Control-Allow-Origin',
+//     'https://fypplayground-client-dn13n5d7y-victoroab.vercel.app/'
+//   )
+//   next()
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
